598bc8284002d5b5fa4cc2c14f00942dcddc7676
karthik-glitch-coder <you@example.com>
2024-06-22 13:34:31 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "This is Namaste React"),
-    React.createElement("h2", {}, "This is Hot Module Replacement"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hi Im H1 tag of child 2"),
-    React.createElement("h2", {}, "Hi Im H2 tag of child 2"),
-  ]),
-]);
+//React Componet
+const Title = () => (
+  <div>
+    <h1>This is Title Component🗣️</h1>
+  </div>
+);
+
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    <h1>This is Heading Componet🚀</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-
+root.render(<HeadingComponent />);
